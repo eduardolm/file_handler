38390020d8d299d09aa466fe087c03027db1fbab
Eduardo Lodi Marzano <lodi001@uol.com.br>
2022-05-19 19:55:21 -0300

    Added regex-way...

--- main/file_handler.py
+++ main/file_handler.py
@@ -1,3 +1,6 @@
+import re
+
+
 class FileHandler:
     log_data: str
 
@@ -31,3 +34,32 @@ class FileHandler:
             self.log_data = fp.read()
 
         return self.log_data
+
+    #  ************************  With REGEX  *****************************
+
+    def separate_data(self) -> tuple:
+        host_pattern = r'[\d]+\.[\d]+\.[\d]+\.[\d]+'
+        user_pattern = r'-\s([A-Za-z0-9_-]+)\s'
+        time_pattern = r'\[[\d].+[\d]\]'
+        request_pattern = r'["].+["]'
+
+        hosts = re.findall(host_pattern, self.log_data)
+        user = re.findall(user_pattern, self.log_data)
+        time = re.findall(time_pattern, self.log_data)
+        request = re.findall(request_pattern, self.log_data)
+
+        return hosts, user, time, request
+
+    def build_regex_final_response(self):
+        response = []
+        data = self.separate_data()
+        for i in range(len(data[0])):
+            output = {
+                'host': data[0][i],
+                'user_name': data[1][i],
+                'time': data[2][i],
+                'request': data[3][i]
+            }
+            response.append(output)
+
+        return response
--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -49,3 +49,33 @@ class TestFileHandler(TestCase):
         self.assertIsInstance(response[0], dict)
         self.assertEqual(979, len(response))
         self.assertEqual(expected, response[0])
+
+    #  ************************  With REGEX  *****************************
+
+    def test_build_regex_final_response(self):
+        self.file_reader.read_file(self.PATH)
+        self.file_reader.extract_lines()
+
+        expected_first = {
+            'host': '146.204.224.152',
+            'user_name': 'feest6811',
+            'time': '[21/Jun/2019:15:45:24 -0700]',
+            'request': '"POST /incentivize HTTP/1.1"'
+        }
+        expected_last = {
+            'host': '30.95.91.251',
+            'user_name': 'larson8319',
+            'time': '[21/Jun/2019:16:02:02 -0700]',
+            'request': '"PUT /one-to-one/whiteboard HTTP/1.0"'
+        }
+
+        response = self.file_reader.build_regex_final_response()
+
+        self.assertIsNotNone(response)
+        self.assertIsInstance(response, list)
+        self.assertIsInstance(response[0], dict)
+        self.assertIsInstance(response[1], dict)
+        self.assertIsInstance(response[2], dict)
+        self.assertIsInstance(response[3], dict)
+        self.assertEqual(expected_first, response[0])
+        self.assertEqual(expected_last, response[-1])
